47bab4af9aa91da84f629036e4526f937e133f1c
Alejandro Blanco <alejandro.b.e@gmail.com>
2014-08-15 20:29:59 +0200

    Add options to the game

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -2,6 +2,7 @@ package ;
 
 import flixel.FlxG;
 import flixel.FlxState;
+import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.util.FlxDestroyUtil;
 
@@ -11,37 +12,56 @@ using flixel.util.FlxSpriteUtil;
  * A FlxState which can be used for the game's menu.
  */
 class MenuState extends FlxState {
-	private var _btnPlay:FlxButton;
+    private var _btnPlay:FlxButton;
+    private var _txtTitle:FlxText;
+    private var _btnOptions:FlxButton;
 
-	/**
-	 * Function that is called up when to state is created to set it up.
-	 */
-	override public function create():Void {
-		_btnPlay = new FlxButton(0, 0, 'Play', clickPlay);
-		_btnPlay.screenCenter();
-		add(_btnPlay);
+    /**
+     * Function that is called up when to state is created to set it up.
+     */
+    override public function create():Void {
+        _txtTitle = new FlxText(20, 0, 0, 'HaxeFlixel\nTutorial\nGame', 22);
+        _txtTitle.alignment = 'center';
+        _txtTitle.screenCenter(true, false);
+        add(_txtTitle);
 
-		super.create();
-	}
+        _btnPlay = new FlxButton(0, 0, 'Play', clickPlay);
+        _btnPlay.x = (FlxG.width / 2) - _btnPlay.width - 10;
+        _btnPlay.y = FlxG.height - _btnPlay.height - 10;
+        add(_btnPlay);
 
-	/**
-	 * Function that is called when this state is destroyed - you might want to
-	 * consider setting all objects this state uses to null to help garbage collection.
-	 */
-	override public function destroy():Void {
-		super.destroy();
+        _btnOptions = new FlxButton(0, 0, 'Options', clickOptions);
+        _btnOptions.x = (FlxG.width / 2) + 10;
+        _btnOptions.y = FlxG.height - _btnOptions.height - 10;
+        add(_btnOptions);
 
-		_btnPlay = FlxDestroyUtil.destroy(_btnPlay);
-	}
+        super.create();
+    }
 
-	/**
-	 * Function that is called once every frame.
-	 */
-	override public function update():Void {
-		super.update();
-	}
+    /**
+     * Function that is called when this state is destroyed - you might want to
+     * consider setting all objects this state uses to null to help garbage collection.
+     */
+    override public function destroy():Void {
+        super.destroy();
 
-	private function clickPlay():Void {
-		FlxG.switchState(new PlayState());
-	}
+        _txtTitle = FlxDestroyUtil.destroy(_txtTitle);
+        _btnPlay = FlxDestroyUtil.destroy(_btnPlay);
+        _btnOptions = FlxDestroyUtil.destroy(_btnOptions);
+    }
+
+    /**
+     * Function that is called once every frame.
+     */
+    override public function update():Void {
+        super.update();
+    }
+
+    private function clickPlay():Void {
+        FlxG.switchState(new PlayState());
+    }
+
+    private function clickOptions():Void {
+        FlxG.switchState(new OptionsState());
+    }
 }
